9c364d46cdc5d751b9498aaf9c52438909042596
Reilan Cad <reilancadubla@gmail.com>
2023-09-13 11:06:39 +0800

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularbasic';
+  reilan = 'pogi';
+
+  name: string = '';
+  listOfNames: string[] = ['Jay Anne', 'Leah', 'Eric', 'Dado', 'Mary Anne'];
+  addName() {
+    console.log('add button is working!');
+    this.listOfNames.push(this.name);
+    console.log(this.listOfNames);
+  }
+  delete(){
+    this.listOfNames.splice(2, 3)
+  }
 }
